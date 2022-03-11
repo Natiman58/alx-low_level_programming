fa0cf20bea35bd8422036a002858fbf51f687649
Natiman58 <natiman58@gmail.com>
2022-03-11 13:52:35 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
