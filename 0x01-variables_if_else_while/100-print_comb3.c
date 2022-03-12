2c8a76fd613058baeff2da2fc97f2e84feec9c41
Natiman58 <natiman58@gmail.com>
2022-03-12 01:45:30 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,18 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	int nums;
-		for (nums = 1; nums <= 99; nums++)
-		{	putchar(nums + '0');
-		if (nums <= 99)
-		{	putchar(',');
-			putchar(' ');
-		if (nums <= 9)
-			putchar('0');
-		}
+{	int tens;
+	int ones;
+		for (tens = 0; tens <= 9; tens++)
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{	putchar(tens + '0');
+				putchar(ones + '0');
+			if (tens <= 8)
+				putchar(',');
+				putchar(' ');
+			}
 		}
 		putchar('\n');
-	return (0);
+		return (0);
 }
