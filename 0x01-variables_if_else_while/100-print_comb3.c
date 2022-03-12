7e2e64496348fff7f2bf8db44fcbf5ea6e659582
Natiman58 <natiman58@gmail.com>
2022-03-12 02:49:50 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,13 @@ int main(void)
 			for (ones = tens + 1; ones <= 9; ones++)
 			{	putchar(tens + '0');
 				putchar(ones + '0');
-			if (tens <= 9)
+			if (tens < 8)
 				{
 				putchar(',');
 				putchar(' ');
 				}
 			}
 		}
-	putchar('\n');
-	return (0);
+			putchar('\n');
+			return (0);
 }
