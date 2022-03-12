44e96f7ba239813a80b0a46bb29080b58afabd94
Natiman58 <natiman58@gmail.com>
2022-03-11 23:53:57 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 {	int i;
 	char letter;
 		for (i = 0; i <= 9; i++)
-		putchar (i);
+			putchar (i);
 		for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
+			putchar(letter);
 	putchar('\n');
 	return (0);
 }
