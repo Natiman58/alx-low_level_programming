c29db9832b1cc03e94751acd01e68ed530187bfa
Natiman58 <natiman58@gmail.com>
2022-03-12 03:15:45 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main-main block
+ * printing all possible combination of 3 digit numbers
+ * The 3 digits must be different
+ * you can only use 'putchar'function 6 times maximum
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{	int hunds;
+	int tens;
+	int ones;
+		for (hunds = 0; hunds <= 9; hunds++)
+		{
+			for (tens = hunds + 1; tens <= 9; tens++)
+			{
+				for (ones = tens + 1; ones <= 9; ones++)
+					putchar(hunds + '0');
+					putchar(tens + '0');
+					putchar(ones + '0');
+			}
+		}
+		putchar('\n');
+		return (0);
+}
