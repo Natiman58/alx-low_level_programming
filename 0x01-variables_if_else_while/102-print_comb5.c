fe81cd399ef5ab70a200c793e18a8036dfed7ec6
Natiman58 <natiman58@gmail.com>
2022-03-12 13:01:17 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main-print all possible combinations of 2 digit numbers.
+ * All numbers should be printed as 2 digits.
+ * The numbers range from 0 to 99
+ * All yout code must be in main fun.
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int tens;
+	int ones;
+		for (tens = 0; tens <= 9; tens++)
+		{
+			for (ones = 0; ones <= 9; ones++)
+			{
+				putchar(tens + '0');
+				putchar(ones + '0');
+			if (!(tens == 9 && ones == 9))
+			{
+				putchar (',');
+				putchar (' ');
+			}
+
+			}
+		}
+	putchar('\n');
+	return (0);
+}
+
