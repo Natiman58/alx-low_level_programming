8f95f22d089a840bba95a09ae08dce6042c39a24
Natiman58 <natiman58@gmail.com>
2022-03-15 04:32:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print "_putchar" followed by new line.
+ * Description: You are not allowed to include standard libs.
+ * Return:Always 0;
+ */
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+		 i++;
+	}
+		_putchar('\n');
+	return (0);
+}
