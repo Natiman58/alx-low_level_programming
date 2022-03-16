eda4f52dc8e5825582bde14e837d43ee10db3003
Natiman58 <natiman58@gmail.com>
2022-03-16 13:48:30 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 	int n1 = 0, n2 = 1, n3, count;
-			for (count = 3; count <= 53; count++)
+			for (count = 3; count <= 50; count++)
 			{
-				if (count <= 53)
+				if (count <= 50)
 				{
 					n3 = n1 + n2;
-					printf("%u", n3);
+					printf("%lu", n3);
 					n1 = n2;
 					n2 = n3;
 					printf(", ");
