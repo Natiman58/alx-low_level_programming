3e671696b1b1df8ecf6a60b452e48d3440da8bf1
Natiman58 <natiman58@gmail.com>
2022-03-16 00:54:05 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,24 +11,24 @@ void times_table(void)
 			for (j = 0; j <= 9; j++)
 			{
 				k = i * j;
-					if (j == 0)
-					{
-						_putchar(k + '0');
-					}
-					else if (k <= 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(k + '0');
-					}
-					else if (k >= 10)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(k / 10 + '0');
-						_putchar(k % 10 + '0');
-					}
+				if (j == 0)
+				{
+					_putchar(k + 48);
+				}
+				else if (k <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + 48);
+				}
+				else if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(k / 10 + 48);
+					_putchar(k % 10 + 48);
+				}
 			}
 			_putchar('\n');
 		}
