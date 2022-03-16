074fbdee5619b4953ef95dbadcb68206f315145e
Natiman58 <natiman58@gmail.com>
2022-03-16 15:09:31 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,26 +8,26 @@
  */
 int main(void)
 {
-	unsigned long int i1 = 0, j1 = 1, i2 = 0, j2 = 2;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
 	unsigned long int hold1, hold2, hold3;
 	int count;
-		printf("%lu, %lu, ", i1, i2);
+		printf("%lu, %lu, ", bk1, bk2);
 	for (count = 2; count < 98; count++)
 	{
-		if (i1 + i2 > LARGEST || j2 > 0 || j1 > 0)
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
 		{
-			hold1 = (i1 + i2) / LARGEST;
-			hold2 = (i1 + i2) % LARGEST;
-			hold3 = j1 + j2 + hold1;
-			j1 = j2, j2 = hold3;
-			i1 = i2, i2 = hold2;
-			printf("%lu%010lu", j2, i2);
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
 		}
 		else
 		{
-			hold2 = i1 + i2;
-			i1 = i2, i2 = hold2;
-			printf("%lu", i2);
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
 		}
 		if (count != 97)
 		{
