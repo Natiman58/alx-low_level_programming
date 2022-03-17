ddc5e26099e007a74e3553fa749e17a00768e152
Natiman58 <natiman58@gmail.com>
2022-03-17 09:32:06 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ *_isdigit - checks if the digit 0 to 9
+ * @c: inputs a number
+ * Return: 1 if a number is b/n 0 and 9, 0 if not
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
