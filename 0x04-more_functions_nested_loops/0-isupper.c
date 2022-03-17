7167c1ebdcb6f54512b706568c793682a0386068
Natiman58 <natiman58@gmail.com>
2022-03-17 07:46:07 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
