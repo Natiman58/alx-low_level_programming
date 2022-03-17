54c0a37af48f08ffce6cab8779db7ea2967bc993
Natiman58 <natiman58@gmail.com>
2022-03-17 14:08:24 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,13 +6,18 @@
  */
 void print_diagonal(int n)
 {
-	int len;
+	int len, gap;
 
 	if (n > 0)
 	{
 		for (len = 0; len <= n; len++)
 		{
-			_putchar('\');
+			for (gap = 0; gap < len; gap++)
+				_putchar(' ');
+			_putchar('\\');
+			if (len == n - 1)
+				continue;
+			_putchar('\n');
 		}
 	}
 	_putchar('\n');
