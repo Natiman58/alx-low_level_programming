6281ad4c7ad28a95b66c38d715bd93883269b71b
Natiman58 <natiman58@gmail.com>
2022-03-17 14:33:40 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_square - prints squares of chx "#"
+ * @size: The size of the square
+ * Returns: 0
+ */
+void print_square(int size)
+{
+	int ver, hor;
+
+	if (size > 0)
+	{
+		for (ver = 0; ver < size; ver++)
+		{
+			for (hor = 0; hor < size; hor++)
+				_putchar('#');
+			if (ver == size - 1)
+				continue;
+			_putchar('\n');
+		}
+
+
+	}
+}
