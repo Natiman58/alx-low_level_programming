d3a0a7bbd87312920a106be7798166c5ca658d9e
Natiman58 <natiman58@gmail.com>
2022-03-17 09:57:29 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_numbers - prints numbers b/n 0 - 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	while (i >= 48 && i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
