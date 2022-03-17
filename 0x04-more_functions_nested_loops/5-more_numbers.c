b8e5268009fef573070b04c884a9321062fcc6c2
Natiman58 <natiman58@gmail.com>
2022-03-17 13:18:00 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,10 @@ void more_numbers(void)
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
+			{
 				_putchar((i / 10) + 48);
-
-			_putchar((i % 10) + 48);
+				_putchar((i % 10) + 48);
+			}
 		}
 	}
 	_putchar('\n');
