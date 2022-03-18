34bab111586aa37fe1d2ed845696989e9ca9235b
Natiman58 <natiman58@gmail.com>
2022-03-18 13:54:28 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,20 +1,37 @@
-#include<stdio.h>
-#include"main.h"
+#include "main.h"
 /**
- * print_number - prints an integer
- * @n: the number
+ * print_number - prints an integer using _putchar() function
+ * @n:input number
+ * @i-tens
+ * @j-digit
+ * @k-positive
  * Return: void
  */
-
 void print_number(int n)
 {
-	if (n < 48 || n >= 48)
+	unsigned int i, j, k = n;
+	double l = 1;
+
+	if (n == 0)
 	{
-		_putchar(n);
+		_putchar('0');
 	}
 	else
 	{
-		return;
+		if (n < 0)
+		{
+			k = n * -1;
+			_putchar('-');
+		}
+		while (l <= k)
+			l *= 10;
+		i = l / 10;
+		while (i >= 1)
+		{
+			j = k / i;
+			_putchar(j + '0');
+			k = (k - (i * j));
+			i /= 10;
+		}
 	}
-	printf("\n");
 }
