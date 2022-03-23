d250f8898843db8d672ca6ee180f4e339039a5e4
Natiman58 <natiman58@gmail.com>
2022-03-23 07:06:59 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * leet - a function that encodes a string to a number
+ * @n: the input string
+ * Return: n
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "44330077ll";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
