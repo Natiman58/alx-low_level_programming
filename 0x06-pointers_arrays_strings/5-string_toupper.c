8bcd957e6da37611faea44b8c61f6521d729ef3e
Natiman58 <natiman58@gmail.com>
2022-03-23 03:12:50 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * string_toupper - a function that changes all
+ * the lower letters cases to uppercase
+ * @s: string input.
+ * Return: the string pointer s
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 97) && (*(s + i) <= 122))
+			*(s + i) = *(s + i) - 32;
+			i++;
+	}
+	return (s);
+}
