c6deb2e5a68c8e3e3585705f0061200bc967ca7d
Natiman58 <natiman58@gmail.com>
2022-03-28 21:46:07 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,19 +8,26 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum1;
-	int sum2;
+	int i, sum, s;
 
-	sum1 = 0;
-	sum2 = 0;
-
-	for (i = 0; i < (size * size); i++)
+	i = 0;
+	sum = 0;
+	s = size * size;
+	while (i < s)
 	{
 		if (i % (size + 1) == 0)
-			sum1 += *(a + 1);
-		if (i % (size - 1) == 0 && i != 0 && i < (size * size - 1))
-			sum2 += *(a + 1);
+			sum += a[i];
+		i++;
 	}
-	printf("%d, %d", sum1, sum2);
+	printf("%d,", sum);
+
+	sum = 0;
+	i = 0;
+	while (i < s)
+	{
+		if (i % (size - 1) == 0 && i != (s - 1) && i != 0)
+			sum += a[i];
+		i++;
+	}
+	printf("%d\n", sum);
 }
