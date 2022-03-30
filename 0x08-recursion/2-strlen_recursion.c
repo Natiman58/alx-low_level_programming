4bcb9f1faf352c52b6ccee57a3210d50da9b2dad
Natiman58 <natiman58@gmail.com>
2022-03-30 00:30:51 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,6 +2,7 @@
 /**
  * _strlen_recursion - prints the length of a string
  * @s: the string to be measured
+ * @l - length of the string
  * Return: the length of the string
  */
 int _strlen_recursion(char *s)
