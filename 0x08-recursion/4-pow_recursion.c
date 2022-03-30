2627e6d6f65d9dd8354d8128bdbec8fb8b7f996c
Natiman58 <natiman58@gmail.com>
2022-03-30 01:19:32 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * _pow_recursion - returns the value of x^y
+ * if y is lessthan 0 it returns -1
+ * @x: base num
+ * @y: power num
+ * Return: result
+ */
+int _pow_recursion(int x, int y)
+{
+	int result;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+
+	result = (x * _pow_recursion(x, y));
+
+	return (result);
+}
