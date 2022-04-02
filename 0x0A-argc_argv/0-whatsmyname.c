484dd7b1979a32221cf4329bf12f8550f1af3613
Natiman58 <natiman58@gmail.com>
2022-04-01 22:53:50 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include"stdio.h"
+#include<stdio.h>
+/**
+ * main - prints the name of the function
+ * @argc: argument count
+ * @argv: array of arguments passed to main as strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
+
