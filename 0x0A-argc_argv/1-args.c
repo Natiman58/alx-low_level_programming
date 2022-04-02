c2d63517ec0a0be237a171bf565290da061e79cd
Natiman58 <natiman58@gmail.com>
2022-04-01 23:10:16 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - prints the number of arguments passed to it
+ * followed by a new line
+ * @argc: the number of arguments
+ * @argv: the array of pointers to strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
