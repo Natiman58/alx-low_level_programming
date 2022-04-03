e7450f103d62263b7dd6654ced3f3afe84ca6b40
Natiman58 <natiman58@gmail.com>
2022-04-03 00:02:38 -0700

    stat lib

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,1 +1,18 @@
-char *_strncpy(char *dest, char *src, int n);
+#include"main.h"
+/**
+ * _strncpy - function copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes from the source
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
