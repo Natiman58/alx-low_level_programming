9abedd6940050fead6fad957ba04b5a88ca65262
Natiman58 <natiman58@gmail.com>
2022-04-04 21:47:01 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ * argstostr - concatnets all the arguments
+ * @ac: argument count
+ * @av: argument value
+ * Return: NULL if ac or av are null or fail
+ * if not return pointer(a) to the the fun
+ */
+char *argstostr(int ac, char **av)
+{
+	char *a;
+	int i, j, total;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0, total = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+			;
+		total++;
+	}
+	total++;
+	a = malloc(total * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*a = av[i][j];
+			a++;
+		}
+		*a = '\n';
+		a++;
+	}
+	return (a);
+}
