d26cd971fce4c0b3cf324644a80c681d94d29946
Natiman58 <natiman58@gmail.com>
2022-04-07 03:18:30 -0700

    coccatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	while (n < len2 && i < (len1 + n))
 		ptr[i++] = s2[j++];
-	while (n > len2 && i < (len1 + n))
+	while (n >= len2 && i < (len1 + len2))
 		ptr[i++] = s2[j++];
 	ptr[i] = '\0';
 
