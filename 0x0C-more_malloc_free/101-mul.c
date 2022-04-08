3f22224833ae65b3d9b73a7bee74f5db1b5169ad
Natiman58 <natiman58@gmail.com>
2022-04-08 08:28:47 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -49,12 +49,10 @@ void errors(void)
  */
 int main(int argc, char *argv[])
 {
-	char *s1;
-	char *s2;
+	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	s1 = argv[1];
-	s2 = argv[2];
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
@@ -78,15 +76,17 @@ int main(int argc, char *argv[])
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
+	}
 	for (i = 0; i < len - 1; i++)
+	{
 		if (result[i])
 			a = 1;
 		if (a)
 			_putchar(result[i] + '0');
+	}
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
 	return (0);
-	}
 }
