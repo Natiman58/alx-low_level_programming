cdb145229ef60a5618b3087b95e5482158b91014
Natiman58 <natnaelmanyazewal14@gmail.com>
2022-04-09 11:05:38 -0700

    edited comments

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  * print_rev - prints a string, in reverse, followed by a new line.
- * @s: the pointer of type char 
+ * @s: a char pointer
  * Return: none
  */
 void print_rev(char *s)
