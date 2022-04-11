27c59ea9d2d0e654d32f4f5e441d024d25a45e99
Natiman58 <natiman58@gmail.com>
2022-04-11 10:30:35 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
