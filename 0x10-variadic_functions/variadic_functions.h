dbfb4f6d57748eb3d33024b470ced608c03f4f52
Natiman58 <natiman58@gmail.com>
2022-04-14 02:42:44 -0700

    variadic function that sums

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARDIC_FUN
+#define VARDIC_FUN
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif
