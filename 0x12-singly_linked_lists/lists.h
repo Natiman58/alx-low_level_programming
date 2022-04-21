2ced9f3a1ded09ead430e6e61a7f4f833c262c41
Natiman58 <natiman58@gmail.com>
2022-04-21 05:41:45 -0700

    fixed the structure

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,15 +8,17 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 
