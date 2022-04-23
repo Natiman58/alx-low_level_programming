ba7214d7d4d93b138304dc18beac97ebdd633801
Natiman58 <natiman58@gmail.com>
2022-04-23 05:39:40 -0700

    rdi

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,10 @@
-
-	global main
-	extern printf
+global main
+extern printf
 
 main:
- mov edi,format
- xor eax,eax
+ mov rdi, format
+ xor eax, eax
  call printf
- mov eax,0
+ mov eax, 0
  ret
 format: db 'Hello, Holberton',0ah
