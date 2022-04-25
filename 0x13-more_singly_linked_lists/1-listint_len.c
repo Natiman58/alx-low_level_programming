52e952e0d5f9a204b198e7991e233c2731ca7fca
Natiman58 <natiman58@gmail.com>
2022-04-24 21:42:51 -0700

    print num of elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - lists the number of elements in a linked list
+ * @h: head pointer of the linked list linstint_t
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
