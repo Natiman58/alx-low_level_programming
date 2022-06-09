aef0a7134ed45002d85f4f3ba1cdeaeb9b901413
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-09 07:37:21 +0300

    print

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* print_dlistint - prints all the elements of doubly linked list forward
+* @h: pointer to the first node of the list
+*
+*Return: number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num_nodes = 0;
+
+	if(!h)
+		return (0);
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_nodes++;
+	}
+	return (num_nodes);
+}
