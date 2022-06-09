f95485a0ae61220176c45e62b672419fb4b34a49
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-09 21:07:08 +0300

    added NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int counter = 1;
-	dlistint_t *temp = NULL, *new_node = 0;
+	dlistint_t *temp = NULL, *new_node = NULL;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL || h == NULL)
