09c79cbbca0827a730e77c270f3712118aee1173
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-09 19:28:05 +0300

    fixed betty

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 /**
  * sum_dlistint - returns the sum of all the data in the doubly linked list
- * @head: pointer to the first node of the linked list 
+ * @head: pointer to the first node of the linked list
  * Return: the sum or 0 if list is empty
  **/
 
 int sum_dlistint(dlistint_t *head)
 {
 	size_t sum = 0;
+
 	if (head == 0)
 		return (0);
 	while (head != NULL)
