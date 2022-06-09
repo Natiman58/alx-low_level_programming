fbd16ab5ad93c066c3ee2af85eb36aa378243979
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-09 22:44:58 +0300

    betty

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		if(*head != NULL)
+		if (*head != NULL)
 		{
 			(*head)->prev = NULL;
 		}
