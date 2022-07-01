11ffa24fd3779a35f3807527b3a552dd1ff63872
Natiman58 <natiman58@gmail.com>
2022-07-01 16:08:04 +0300

    all

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * free_list - frees the linked list of nodes
+ * @head: pointer to the first element of the linked list
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->key);
+		free(head->value);
+		free(head);
+		head = temp;
+	}
+}
+
+/**
+ * hash_table_delete - deletes the hash table
+ * @ht: hash table to be deleted
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+		free_list(ht->array[i]);
+	free(ht->array);
+	free(ht);
+}
