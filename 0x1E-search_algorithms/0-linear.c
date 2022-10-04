a4b3e0da8ae8ed1b0a3d0391cedff5d59ebec604
natiman58 <natiman58@gmail.com>
2022-10-04 08:20:25 +0300

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+/**
+ * linear_search - uses liner search algorithm to find an element
+ * @array: the array to look into
+ * @value: the value being looked for
+ *
+ * Return: index of the value
+ * else -1
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!value || !array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+	{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef STHIO_H
+#define STDIO_H
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+
+
+#endif
+
